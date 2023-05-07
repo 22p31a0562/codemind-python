6f1fba92913d6c3cfb789800bbef261e01c4719c
Peluri Ramya <119730513+22p31a0562@users.noreply.github.com>
2023-05-07 14:58:46 +0530

    a=int(input()) b=int(input()) for num in range(a, b + 1):     temp = num     reverse = 0
        while(temp > 0):
            Reminder = temp % 10
            reverse = (reverse * 10) + Reminder
            temp = temp //10
    
        if(num == reverse):
            print("%d " %num, end = '')

---
+++ Program_to_print_all_palindromes_in_a_given_range_.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+for num in range(a, b + 1):
+    temp = num
+    reverse = 0
+    
+    while(temp > 0):
+        Reminder = temp % 10
+        reverse = (reverse * 10) + Reminder
+        temp = temp //10
+
+    if(num == reverse):
+        print("%d " %num, end = '')
